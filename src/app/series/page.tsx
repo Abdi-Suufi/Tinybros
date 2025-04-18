2b8cb02d53b51208b5e5e40e24b37336c8bc2777
Abdi Rahman Suufi <93520190+Abdi-Suufi@users.noreply.github.com>
2025-04-18 16:39:19 +0100

    Update page.tsx

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { fetchTVShows, getImageUrl, TMDBShow } from '@/lib/tmdb';
-import Link from 'next/link';
 import Loading from '@/components/Loading';
 
 export default function SeriesPage() {
